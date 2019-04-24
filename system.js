ce1680941a26230d895c25caf85484dfdfbe4f18
andrianadi888 <44861381+andrianadi888@users.noreply.github.com>
2019-04-24 18:01:03 +0700

    Add files via upload

---
+++ system.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#keylock,.keylock").attr("href") != "https://aditema.blogspot.com/") {
+        window.location.href = "https://aditema.blogspot.com/"; }});
+//]]>
+</script>
